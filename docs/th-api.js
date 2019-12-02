8b8bd0dc2b9c99585f74b05fe949d16ab17401b1
AStavrou16 <astavrou@uclan.ac.uk>
2019-12-02 18:04:55 +0200

    Skip Feature

--- docs/th-api.js
+++ docs/th-api.js
@@ -1,60 +1,19 @@
+/*
+ISSUES:
+LOCATION
+TEXT ANSWER IS NOT WORKING
+
+ */
 const TH_BASE_URL = "https://codecyprus.org/th/api/"; // the true API base url
 const TH_TEST_URL = "https://codecyprus.org/th/test-api/"; // the test API base url
-const Test_Question = "question?question-type=";
 
-/**
- * An asynchronous function to realize the functionality of getting the available 'treasure hunts' (using /list) and
- * processing the result to update the HTML with a bullet list with the treasure hunt names and descriptions. Also,
- * for each treasure hunt in the bullet list, a link is shown to trigger another function, the 'select'.
- * @return {Promise<void>}
- */
-async function doList() {
-
-    // call the web service and await for the reply to come back and be converted to JSON
-    const reply = await fetch(TH_BASE_URL + "list");
-    const json = await reply.json();
-
-    // identify the spinner, if available, using the id 'loader'...
-    let spinner = document.getElementById("loader");
-    // .. and stop it (by hiding it)
-    spinner.hidden = true;
-
-    // access the "treasureHunts" array on the reply message
-    let treasureHuntsArray = json.treasureHunts;
-    let listHtml = "<ul>"; // dynamically form the HTML code to display the list of treasure hunts
-    for(let i = 0; i < treasureHuntsArray.length; i++) {
-        listHtml += // each treasure hunt item is shown with an individual DIV element
-            "<li>" +
-            "<b>" + treasureHuntsArray[i].name + "<b><br>" + // the treasure hunt name is shown in bold...
-            "<i>" + treasureHuntsArray[i].description + "</i><br>" + // and the description in italics in the following line
-            "<a href=\"javascript:select(\'" + treasureHuntsArray[i].uuid + "\')\">Start</a>" + // and the description in italics in the following line
-            "</li>";
-    }
-    listHtml += "</ul>";
-    // update the DOM with the newly created list
-    document.getElementById("treasureHunts").innerHTML = listHtml;
-}
-
-/**
- * This function is called when a particular treasure hunt is selected. This is merely a placeholder as you're expected
- * to realize this function-or an equivalent-to perform the necessary actions after a treasure hunt is selected.
- *
- * @param uuid this is the argument that corresponds to the UUID of the selected treasure hunt.
- * @return {Promise<void>}
- */
-async function select(uuid) {
-    // For now just print the selected treasure hunt's UUID. Normally, you're expected to guide the user in entering
-    // their name etc. and proceed to calling the '/start' command of the API to start a new session.
-    console.log("Selected treasure hunt with UUID: " + uuid);
-    // todo add your own code ...
-}
 // Function to call a list of Treasure Hunts.
 function callList() {
     let treasureHuntsElement = document.getElementById("treasureHunts");
     fetch("https://codecyprus.org/th/api/list")
         .then(response => response.json()) //Parse JSON text to JavaScript object
         .then(jsonObject => {
-            let treasureHunts = jsonObject.treasureHunts;
+            let treasureHunts = jsonObject['treasureHunts'];
             console.log(treasureHunts);
             for (let i = 0; i < treasureHunts.length; i++) {
                 treasureHuntsElement.innerHTML += "<a href='start.html'>" + treasureHunts[i].name + "</a><br>"
@@ -73,9 +32,9 @@ function start(){
         .then(jsonObject => {
                 let startSession = jsonObject;
                 if(startSession.status ==='OK'){
-                    window.location.href="question.html?session="+startSession.session;
+                    window.location.href="question.html?session="+startSession['session'];
                 }else{
-                    startElement.innerHTML +=startSession.errorMessages+"<br>";
+                    startElement.innerHTML +=startSession['errorMessages']+"<br>";
                 }
         }
         );
@@ -93,39 +52,39 @@ function getParameter(parameter) {
 }
 function handleQuestionLibrary(json) {
     let questionElement = document.getElementById("errorMessages");
-
+console.log(json);
     if(json.status === "OK") {
-        document.getElementById('question').innerHTML = json.questionText;
+        document.getElementById('question').innerHTML = json['questionText'];
 
-        if (json.questionType === 'BOOLEAN') {
+        if (json['questionType'] === 'BOOLEAN') {
             document.getElementById('text').style.display = 'none';
             document.getElementById('boolean').style.display = 'block';
             document.getElementById('integer').style.display = 'none';
             document.getElementById('decimal').style.display = 'none';
             document.getElementById('mcq').style.display = 'none';
         }
-        else if (json.questionType === 'TEXT'){
+        else if (json['questionType'] === 'TEXT'){
             document.getElementById('text').style.display = 'block';
             document.getElementById('boolean').style.display = 'none';
             document.getElementById('integer').style.display = 'none';
             document.getElementById('decimal').style.display = 'none';
             document.getElementById('mcq').style.display = 'none';
         }
-        else if (json.questionType === 'NUMERIC'){
+        else if (json['questionType'] === 'NUMERIC'){
             document.getElementById('text').style.display = 'none';
             document.getElementById('boolean').style.display = 'none';
             document.getElementById('integer').style.display = 'none';
             document.getElementById('decimal').style.display = 'block';
             document.getElementById('mcq').style.display = 'none';
         }
-        else if (json.questionType === 'MCQ'){
+        else if (json['questionType'] === 'MCQ'){
             document.getElementById('text').style.display = 'none';
             document.getElementById('boolean').style.display = 'none';
             document.getElementById('integer').style.display = 'none';
             document.getElementById('decimal').style.display = 'none';
             document.getElementById('mcq').style.display = 'block';
         }
-        else if (json.questionType === 'INTEGER'){
+        else if (json['questionType'] === 'INTEGER'){
             document.getElementById('text').style.display = 'none';
             document.getElementById('boolean').style.display = 'none';
             document.getElementById('integer').style.display = 'block';
@@ -134,18 +93,57 @@ function handleQuestionLibrary(json) {
         }
     }
     else{
-        questionElement.innerHTML +=json.errorMessages+"<br>";
+        questionElement.innerHTML +=json['errorMessages']+"<br>";
     }
-    if (json.completed===false){
-        LoadQuestion();
+    if (json['completed']===true){
+        //MOVE TO SCORE
+    }
+    if (navigator.geolocation) {
+        if(json['requiresLocation']===true) {
+            navigator.geolocation.getCurrentPosition(sendLocation);
+        }
+    }
+    else {
+        alert("Geolocation is not supported by your browser.");
     }
 }
+
+function sendLocation(position) {
+    let lat = position.coords.latitude;
+    let lng = position.coords.longitude;
+    fetch(TH_BASE_URL+"location?session="+ getParameter('session') + "&latitude="+ lat + "&longitude=" + lng)
+        .then(response => response.json())
+        .then(json => {
+           if (json.status=== 'OK'){
+               //????????
+           }
+           else{
+               let error = document.getElementById("errorMessages");
+               error.innerHTML += json['errorMessages']
+           }
+        });
+}
+
 
 function loadAnswer(ans) {
     fetch(TH_BASE_URL+"answer?session="+ getParameter('session') + "&answer="+ ans)
         .then(response => response.json())
-        .then(json => handleQuestionLibrary(json))
+        .then(json => handleQuestionLibrary(json));
+     window.location.reload();
+}
+//SKIP
+function skip() {
+    fetch(TH_BASE_URL+"skip?session="+ getParameter('session'))
+        .then(response => response.json())
+        .then(json => {
+            if (json.status === 'OK') {
+                window.location.href = "question.html?session=" + getParameter(['session']);
+            } else {
+                let error = document.getElementById("errorMessages");
+                error.innerHTML += json['errorMessages']
+            }
+        }
+        );
 }
 
-LoadQuestion();
 
